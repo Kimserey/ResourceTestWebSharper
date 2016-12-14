9ffaf00b44d61f62093536bf2a4464d7e7697310
Kimserey Lam <kimserey.lam@groomgy.com>
2016-12-14 23:51:27 +0000

    added jwt

--- ResourceTestWebSharper/Main.fs
+++ ResourceTestWebSharper/Main.fs
@@ -27,32 +27,71 @@ module Root =
     open Microsoft.Owin.FileSystems
     open WebSharper.Owin
     open WebSharper.Web
+    open System
+    open System.Collections
+    open System.Collections.Generic
+    open System.Security.Cryptography
+    open Newtonsoft.Json
 
-    [<Rpc>]
-    let get() =
-        let ctx = Remoting.GetContext()
-        let owinCtx = unbox<OwinContext> <| ctx.Environment.Item("OwinContext")
+    module Token =
         
-        async { 
-            return "Hello world" 
-        }
+        type JwtPayload =
+            {
+                [<JsonProperty "iss">]
+                Issuer: string
+                [<JsonProperty "sub">]
+                Subject: string
+                [<JsonProperty "exp">]
+                Expiry: DateTime
+                [<JsonProperty "iat">]
+                IssuedAtTime: DateTime
+                [<JsonProperty "jti">]
+                Id: string
+            }
+
+        // Better not to be stored in code 256 bytes encoded in base64
+        let privateKey = 
+            "Lu0YxSQm4UNb+MG1hZA1xhMJaaenYVMVm8U1I4N7Hm7BdkATU05XZz02y1bAvrrf/6ie1ZRo/6Mb1Oqxg0QJs2QgCBpClD/xup/2AZ3mBetJ0YxDIozYsalGiifNpAKNAayOUz+VgEYgQBh8lOsdiA9mTvr1g0VeNlUAktcTdzb9SqleZnnVZKA8BPWff/gcSXtbFtxEnJM5YJJPayEMyDf1HHfxUmC/0Zu6aQIorxe8puwfYmXgNhzJdAgNT65exM3XBKNzBAv/GcOvw0xIkhqkcoBBYq7Avd/vXjVSIaFmF5tyyRNrkTGkEJ34pl1qWgeU9tHrxNQ1rdmutDSLZg=="
+
+        let generateToken (subject: string) (expiry: DateTime) =
+            let payload = 
+                {
+                    Issuer = "com.kimserey"
+                    Subject = subject
+                    Expiry = expiry
+                    IssuedAtTime = DateTime.UtcNow
+                    Id = Guid.NewGuid().ToString("N")
+                }
+            Jose.JWT.Encode(JsonConvert.SerializeObject(payload), Convert.FromBase64String(privateKey), Jose.JwsAlgorithm.HS256);
+
+    module Rpcs =
+        open Token
+
+        [<Rpc>]
+        let token() =
+            async { 
+                return generateToken "some_user" (DateTime.UtcNow.AddMinutes(2.))
+            }
 
     [<JavaScript>]
     module Client =
         open WebSharper.JavaScript
         open WebSharper.UI.Next.Client
 
-        let onClick() =
+        let onClick (callback: string -> unit) =
             async {
-                let! text = get()
-                Console.Log text
+                let! text = Rpcs.token()
+                callback text
             } |> Async.StartImmediate
 
         let page() =
+            let token = Var.Create ""
+
             divAttr 
                 [ attr.``class`` "box" ] 
                 [ text "Hello world"
-                  div [ Doc.Button "Rpc" [] onClick ] ]
+                  div [ Doc.TextView token.View ] 
+                  div [ Doc.Button "Rpc" [] (fun () -> onClick (Var.Set token)) ] ]
 
     let sitelet = Application.SinglePage(fun ctx -> Content.Page(MainTemplate.Doc("Test", [ client <@ Client.page() @> ])))
 
--- ResourceTestWebSharper/Scripts/WebSharper/ResourceTestWebSharper.js
+++ ResourceTestWebSharper/Scripts/WebSharper/ResourceTestWebSharper.js
@@ -1,21 +1,18 @@
 (function()
 {
- var Global=this,Runtime=this.IntelliFactory.Runtime,Concurrency,Remoting,AjaxRemotingProvider,console,UI,Next,Doc,List,AttrProxy,ResourceTestWebSharper,Root,Client,T;
+ var Global=this,Runtime=this.IntelliFactory.Runtime,Concurrency,Remoting,AjaxRemotingProvider,UI,Next,Var,Doc,List,AttrProxy,ResourceTestWebSharper,Root,Client,Var1,T;
  Runtime.Define(Global,{
   ResourceTestWebSharper:{
    Root:{
     Client:{
-     onClick:function()
+     onClick:function(callback)
      {
       var arg00;
       arg00=Concurrency.Delay(function()
       {
        return Concurrency.Bind(AjaxRemotingProvider.Async("ResourceTestWebSharper:0",[]),function(_arg1)
        {
-        if(console)
-         {
-          console.log(_arg1);
-         }
+        callback(_arg1);
         return Concurrency.Return(null);
        });
       });
@@ -25,15 +22,20 @@
      },
      page:function()
      {
-      var arg20,arg201;
-      arg201=function()
+      var token,arg20,arg201,arg202;
+      token=Var.Create("");
+      arg20=List.ofArray([Doc.TextView(token.get_View())]);
+      arg202=function()
       {
-       return Client.onClick();
+       return Client.onClick(function(arg10)
+       {
+        return Var1.Set(token,arg10);
+       });
       };
-      arg20=List.ofArray([Doc.Button("Rpc",Runtime.New(T,{
+      arg201=List.ofArray([Doc.Button("Rpc",Runtime.New(T,{
        $:0
-      }),arg201)]);
-      return Doc.Element("div",List.ofArray([AttrProxy.Create("class","box")]),List.ofArray([Doc.TextNode("Hello world"),Doc.Element("div",[],arg20)]));
+      }),arg202)]);
+      return Doc.Element("div",List.ofArray([AttrProxy.Create("class","box")]),List.ofArray([Doc.TextNode("Hello world"),Doc.Element("div",[],arg20),Doc.Element("div",[],arg201)]));
      }
     }
    }
@@ -44,15 +46,16 @@
   Concurrency=Runtime.Safe(Global.WebSharper.Concurrency);
   Remoting=Runtime.Safe(Global.WebSharper.Remoting);
   AjaxRemotingProvider=Runtime.Safe(Remoting.AjaxRemotingProvider);
-  console=Runtime.Safe(Global.console);
   UI=Runtime.Safe(Global.WebSharper.UI);
   Next=Runtime.Safe(UI.Next);
+  Var=Runtime.Safe(Next.Var);
   Doc=Runtime.Safe(Next.Doc);
   List=Runtime.Safe(Global.WebSharper.List);
   AttrProxy=Runtime.Safe(Next.AttrProxy);
   ResourceTestWebSharper=Runtime.Safe(Global.ResourceTestWebSharper);
   Root=Runtime.Safe(ResourceTestWebSharper.Root);
   Client=Runtime.Safe(Root.Client);
+  Var1=Runtime.Safe(Next.Var1);
   return T=Runtime.Safe(List.T);
  });
  Runtime.OnLoad(function()
